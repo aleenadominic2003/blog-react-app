94acb91549c6ae3324baf7ca2ec7c18aef4837a0
aleenadominic2003 <aleenadominic16@gmail.com>
2026-07-16 01:26:09 +0530

    Sign in done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+       
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
