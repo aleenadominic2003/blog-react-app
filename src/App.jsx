ab6d1d465a29cc54bf1d950e9ed49b12f9d5d46c
aleenadominic2003 <aleenadominic16@gmail.com>
2026-07-16 01:58:00 +0530

    View all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import CreatePost from "./components/CreatePost";
+import ViewAll from "./components/ViewAll";
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/create" element={<CreatePost />} /> 
+        <Route path="/viewall" element={<ViewAll />} />
       </Routes>
     </BrowserRouter>
   );
